da9cd55681c2b03c63cac1e3c1a29a94118a916d
40gilad <40gilad.shenkar@gmail.com>
2024-07-22 12:44:23 +0300

    added ShootingMethods class as singleton to use in concreteWeaponsModel. both async and pooling of hammer and boomerang works fine

--- project/Assets/Scripts/Implementations/Models/SC_BoomerangWeaponModel.cs
+++ project/Assets/Scripts/Implementations/Models/SC_BoomerangWeaponModel.cs
@@ -10,47 +10,10 @@ public class SC_BoomerangWeaponModel : ConcreteWeaponModel
 
     protected override void CustomizeShoot(GameObject weapon, float direction)
     {
-        _ = ShootBoomerang(weapon, direction);
+        _ = ShootingMethods.Instance().ShootBoomerang(weapon, direction,xSpeed,destroyTime);
 
     }
 
-    private async Task ShootBoomerang(GameObject weapon, float direction)
-    {
-        Vector3 startPosition = weapon.transform.position;
-        Rigidbody2D rb = weapon.GetComponent<Rigidbody2D>();
-        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-        // Rotation speed in degrees per second
-        float rotationSpeed = 360f;
-
-        // Move to the right along the x-axis
-        rb.velocity = new Vector2(xSpeed * direction, 0);
-        float travelTime = destroyTime / 2;
-
-        // Start the movement and rotation
-        float elapsedTime = 0f;
-        while (elapsedTime < travelTime)
-        {
-            weapon.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
-            elapsedTime += Time.deltaTime;
-            await Task.Yield();
-        }
-
-        // Move back to the original position
-        rb.velocity = new Vector2(-xSpeed * direction, 0);
-
-        // Continue the movement and rotation
-        elapsedTime = 0f;
-        while (elapsedTime < travelTime)
-        {
-            weapon.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
-            elapsedTime += Time.deltaTime;
-            await Task.Yield();
-        }
-
-        // Stop and return the weapon to the pool
-        rb.velocity = Vector2.zero;
-        PoolManager.Instance.ReturnObjectToPool(weapon);
-    }
 }
 
 
---
+++ project/Assets/Scripts/ShootingMethods.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ShootingMethods
+{
+
+    private static ShootingMethods _instance;
+    private ShootingMethods()
+    {
+    }
+
+    public static ShootingMethods Instance()
+    {
+        if (_instance == null)
+            _instance = new ShootingMethods();
+        return _instance;
+    }
+
+    public async Task Shoot(GameObject weapon, float direction, float xSpeed, float destroyTime, Func<GameObject, float, float, float, Task> shootFunction)
+    {
+        await shootFunction(weapon, direction, xSpeed, destroyTime);
+        PoolManager.Instance.ReturnObjectToPool(weapon);
+    }
+
+    public async Task ShootBoomerang(GameObject weapon, float direction,
+        float xSpeed, float destroyTime)
+    {
+        Vector3 startPosition = weapon.transform.position;
+        Rigidbody2D rb = weapon.GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+        // Rotation speed in degrees per second
+        float rotationSpeed = 360f;
+
+        // Move to the right along the x-axis
+        rb.velocity = new Vector2(xSpeed * direction, 0);
+        float travelTime = destroyTime / 2;
+
+        // Start the movement and rotation
+        float elapsedTime = 0f;
+        while (elapsedTime < travelTime)
+        {
+            weapon.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            await Task.Yield();
+        }
+
+        // Move back to the original position
+        rb.velocity = new Vector2(-xSpeed * direction, 0);
+
+        // Continue the movement and rotation
+        elapsedTime = 0f;
+        while (elapsedTime < travelTime)
+        {
+            weapon.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            await Task.Yield();
+        }
+        PoolManager.Instance.ReturnObjectToPool(weapon);
+
+    }
+
+    public async Task ShootBasic(GameObject weapon, float direction,
+    float xSpeed, float destroyTime)
+    {
+        Rigidbody2D rb = weapon.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(xSpeed * direction, 0);
+        await Task.Delay((int)(destroyTime * 1000));
+        PoolManager.Instance.ReturnObjectToPool(weapon);
+    }
+}
